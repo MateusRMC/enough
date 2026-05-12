caabac0f9c39fca875210c7e4839927cc0ce721b
Mateus Rocha <mateus@Mattbook.local>
2026-05-11 23:55:27 -0400

    start

--- app/page.js
+++ app/page.js
@@ -1,66 +1,31 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.js file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <>
+      <div className="hero">
+        <h1 className="headline">
+          We believe in non-harmful and responsible <span style={{ color: "#FF8D28" }}>media</span>{" "}
+          for everyone
+        </h1>
+        <Link href="/" className="hero-cta">
+          Submit a story
+        </Link>
+      </div>
+      <div className="stories">
+        <div className="story">
+          <h1>ACME developed a way to save the flowers in west ontario</h1>
+          <img src="/story.png" />
         </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="story">
+          <h1>ACME developed a way to save the flowers in west ontario</h1>
+          <img src="/story.png" />
         </div>
-      </main>
-    </div>
+        <div className="story">
+          <h1>ACME developed a way to save the flowers in west ontario</h1>
+          <img src="/story.png" />
+        </div>
+      </div>
+    </>
   );
 }
